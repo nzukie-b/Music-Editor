3a87bcfef4f31c94b859fe820b3054858706826d
nzukie <rushlinerise>
2017-03-30 16:18:05 -0400

    Scrollable Gui, Mouse only.

--- src/cs3500/music/view/GuiViewFrame.java
+++ src/cs3500/music/view/GuiViewFrame.java
@@ -1,14 +1,12 @@
 package cs3500.music.view;
 
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.KeyListener;
 
 import java.util.TreeMap;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
+
+import javax.swing.*;
 
 /**
  * A skeleton Frame (i.e., a window) in Swing, used to draw the music on a "staff"
@@ -32,17 +30,21 @@ public class GuiViewFrame extends javax.swing.JFrame implements IMusicView {
     this.shtPanel = new SheetPanel(data);
     this.pianoPanel = new PianoPanel(data);
 
-    shtPanel.setBackground(Color.white);
-    pianoPanel.setBackground(Color.gray);
+    shtPanel.setBackground(Color.WHITE);
+    pianoPanel.setBackground(Color.GRAY);
 
-    JScrollPane scrollBar = new JScrollPane(shtPanel);
+    JScrollPane scrollPane = new JScrollPane(shtPanel,
+            ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.getContentPane().add(shtPanel);
+    this.getContentPane().add(scrollPane);
 
-    this.add(shtPanel, BorderLayout.NORTH);
-    this.getContentPane().add(scrollBar);
+
+    this.add(scrollPane, BorderLayout.NORTH);
+    //this.getContentPane().add(scrollPane);
     this.add(pianoPanel, BorderLayout.CENTER);
+
     this.setSize(getPreferredSize());
 
     this.setVisible(true);
@@ -52,6 +54,7 @@ public class GuiViewFrame extends javax.swing.JFrame implements IMusicView {
 
   @Override
   public Dimension getPreferredSize() {
+    //return new Dimension((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(), 1000);
     return new Dimension(Math.max(500, shtPanel.getWidth()), 500 + shtPanel.getHeight());
   }
 
